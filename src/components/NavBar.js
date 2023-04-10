122178aa0754c476b1170dc41fdbed0bea1be08e
+ <tidydata@outlook.com>
2023-04-10 13:42:29 -0400

    Submit react hooks JSX lab!

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,10 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
+  return <nav>
+    <a href="#home">"Home"</a>
+    <a href="#about">"About"</a>
+  </nav>
   return <nav>NavBar</nav>;
 }
 
